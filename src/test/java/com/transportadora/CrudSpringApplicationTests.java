1e048e12b32afb342cde1e06a1bac0cdb01fb5e2
ricardodelvecchio <ric.vecchio@gmail.com>
2025-12-28 23:10:25 -0300

    Initial commit - projeto base copiado do transp-api-crud-spring

---
+++ src/test/java/com/transportadora/CrudSpringApplicationTests.java
@@ -0,0 +1,12 @@
+package com.transportadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+}
